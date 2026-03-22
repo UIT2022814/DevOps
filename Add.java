111abfabeacab171bdc3ce4e2a9a3493ecb887b4
UIT2022814 <ruchi.bhatkar@cumminscollege.in>
2026-03-22 15:16:09 +0530

    Change on feature1 branch

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
 public class Add {
     
     int addNum(int x, int y) {
-        System.out.println("Code for addition");
+        System.out.println("Code for addition of 2 numbers");
         return x + y;
     }
 }
